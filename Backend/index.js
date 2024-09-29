43636cc888cdb5c3ab7cf3940bf9afeacddeaa8c
dilusha07 <dilushasandaruwani07@gmail.com>
2024-09-29 22:04:30 +0530

    Add Pin Router

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const pinRoute = require("./routes/pinsRoute");
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 mongoose 
  .connect(process.env.MONGO_URL)   
  .then(() => console.log("MongoDB connected!"))
  .catch(err => console.log(err));
+
+ app.use(`/api/pins`, pinRoute);
+ 
+  
 
 app.listen(3001, ()=>{
     console.log("Backend server is running!");
